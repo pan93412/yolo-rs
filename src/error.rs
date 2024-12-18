288609cf096ceca86bbfa3c309ed2ebd5a85a451
Yi-Jyun Pan <pan93412@gmail.com>
2024-12-18 20:48:20 +0800

    fix: Implement error for YoloError

--- src/error.rs
+++ src/error.rs
@@ -1,9 +1,15 @@
 //! The errors of the Yolo crate.
 
+#[derive(thiserror::Error, Debug)]
 pub enum YoloError {
+    #[error("build session: {0}")]
     OrtSessionBuildError(ort::Error),
+    #[error("load session: {0}")]
     OrtSessionLoadError(ort::Error),
+    #[error("load model: {0}")]
     OrtInputError(ort::Error),
+    #[error("run inference: {0}")]
     OrtInferenceError(ort::Error),
+    #[error("extract sensor: {0}")]
     OrtExtractSensorError(ort::Error),
 }
