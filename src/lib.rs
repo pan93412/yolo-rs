2ce794813e5fd5d249c489707aefe598c8bd0542
Yi-Jyun Pan <pan93412@gmail.com>
2024-12-18 20:49:39 +0800

    fix: Properties in BoundingBox should be public

--- src/lib.rs
+++ src/lib.rs
@@ -15,10 +15,10 @@ use ort::inputs;
 
 #[derive(Debug, Clone, Copy)]
 pub struct BoundingBox {
-    x1: f32,
-    y1: f32,
-    x2: f32,
-    y2: f32,
+    pub x1: f32,
+    pub y1: f32,
+    pub x2: f32,
+    pub y2: f32,
 }
 
 #[derive(Debug, Clone)]
